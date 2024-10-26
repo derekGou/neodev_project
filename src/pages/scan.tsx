978847effa87362b92daee9f545baa71876fb402
derekGou <derekgou19@gmail.com>
2024-10-26 18:12:46 -0400

    help

--- src/pages/scan.tsx
+++ src/pages/scan.tsx
@@ -11,15 +11,23 @@ import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { useLoader } from '@react-three/fiber';
+import { IoIosClose } from "react-icons/io";
 
 const ObjModel = ({ url }) => {
     const obj = useLoader(OBJLoader, url);
   
-    return <primitive object={obj} />;
+    return <primitive object={obj} scale={[2, 2, 2, 2]}/>;
   };
   
   const ObjViewer = ({ url }) => (
-    <Canvas>
+    <Canvas style={{
+        width: '100%', // or a specific width, like '800px'
+        height: '100%', // Adjust height here to make it larger
+        boxSizing: 'content-box',
+        border: '1px solid white',
+        backgroundClip: '#012',
+        cursor: 'default'
+    }}>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <ObjModel url={url} />
@@ -31,9 +39,16 @@ const ObjModel = ({ url }) => {
 function Scan() {
     const [uid, setUid] = useState('')
     const [currUser, setCurrUser] = useState<any[]>([])
-    const [files, setFiles] = useState([])
+    const [files, setFiles] = useState([['akudwh', '23MB'],['akudwh', '23MB'],['akudwh', '23MB'],['akudwh', '23MB'],['akudwh', '23MB'],['akudwh', '23MB']])
     const [link, setLink] = useState<JSX.Element>()
     const [frame, setFrame] = useState<any>()
+    const [fileShow, setFileShow] = useState<JSX.Element[]>([])
+    const [currFile, setCurrFile] = useState<string>('')
+    const [show3d, setShow3d] = useState<string>('none')
+
+    useEffect(()=>{
+    }, [show3d])
+
     onAuthStateChanged(auth, (user) => {
         if (user) {
             // User is signed in, see docs for a list of available properties
@@ -73,7 +88,7 @@ function Scan() {
         var rows: JSX.Element[] = []
         for (let i = 0; i<files.length; i++){
             rows.push(
-                <div className="p-4 gap-4 flex flex-row border-[1px] border-white items-center align-center">
+                <div key={i} className="p-4 gap-4 flex flex-row border-[1px] border-white items-center align-center">
                     <div className="p-2 bg-[#234] cursor-pointer hover:brightness-200">
                         <FaFileDownload/>
                     </div>
@@ -82,12 +97,23 @@ function Scan() {
                         <p className="text-[0.6rem]">{files[i][1]}</p>
                     </div>
                     <div className="p-2 bg-[#234] cursor-pointer hover:brightness-200">
+                        <IoIosClose/>                            
+                    </div>
+                    <div className="p-2 bg-[#234] cursor-pointer hover:brightness-200" onClick={(e)=>{
+                        setFrame('babababa')
+                        console.log('haha')
+                    }}>
                         <BiSolidShow/>                            
                     </div>
                 </div>
             )
         }
+        setFileShow(rows)
     }, [files])
+
+    const garrr = () => {
+        console.log('har')
+    }
 
     return (
         <>
@@ -109,8 +135,26 @@ function Scan() {
                         </button>
                     </div>
                 </div>
-                <div className="flex flex-col border-2 border-white grow">
-                <ObjViewer url="texturedMesh.obj"/>
+                <div className="flex flex-col border-2 border-white grow overflow-y-scroll">
+                    <div className="p-4 gap-4 flex flex-row border-[1px] border-white items-center align-center">
+                        <div className="p-2 bg-[#234] cursor-pointer hover:brightness-200">
+                            <FaFileDownload/>
+                        </div>
+                        <div className="flex flex-col grow">
+                            <p className="text-[0.8rem]">asd</p>
+                            <p className="text-[0.6rem]">asd</p>
+                        </div>
+                        <div className="p-2 bg-[#234] cursor-pointer hover:brightness-200">
+                            <IoIosClose/>                            
+                        </div>
+                        <div className="p-2 bg-[#234] cursor-pointer hover:brightness-200" onClick={(e)=>{
+                            setFrame('babababa')
+                            console.log('haha')
+                        }}>
+                            <BiSolidShow/>                            
+                        </div>
+                    </div>
+                    {fileShow}
                 </div>
                 <button className="w-full" onClick={()=>{
                     signOut(auth).then(() => {
@@ -119,6 +163,19 @@ function Scan() {
                         // An error happened.
                     });
                 }}>Logout</button>
+            </div>
+            <div style={{display:show3d}} className="w-screen h-screen bg-[#012] p-36 z-20 fixed">
+                <div className="flex w-full border-[1px] border-white box-content flex-row">
+                    <p className="grow">{currFile}</p>
+                    <div className="cursor-pointer" onClick={()=>{
+                        setShow3d('none')
+                    }}>
+                        <IconContext.Provider value={{size:'2rem', color:'white'}}>
+                            <IoIosClose/>
+                        </IconContext.Provider>
+                    </div>
+                </div>
+                <ObjViewer url="texturedMesh.obj"/>
             </div>
         </>
     )
