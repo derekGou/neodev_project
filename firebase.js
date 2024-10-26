8c82dbc9b067cd4eeac09d49ff2ae5ffe9972741
derekGou <derekgou19@gmail.com>
2024-10-26 11:02:19 -0400

    firebase import, ts config patches

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzjPpiJ5KfOUP_shUTghCWJAR9_XzaVn8",
+  authDomain: "neodev-148c0.firebaseapp.com",
+  projectId: "neodev-148c0",
+  storageBucket: "neodev-148c0.appspot.com",
+  messagingSenderId: "933942551634",
+  appId: "1:933942551634:web:cd9ce176ba78378d076b8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
